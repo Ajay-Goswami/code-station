687ac1a8a41daaa574b90b706132da697f6811a2
Ajay <goswamiajay2002@gmail.com>
2022-02-10 12:24:50 +0530

    Add header and topbar section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import TopBar from "./components/topbar/TopBar";
 import React from "react";
-import Home from "./pages/homepage/Home";
-import Detail from "./pages/listingdetail/Detail";
-import CreateListing from "./pages/create-listing/CreateListing";
+import Header from "./components/header/Header";
+// import Home from "./pages/homepage/Home";
+// import Detail from "./pages/listingdetail/Detail";
+// import CreateListing from "./pages/create-listing/CreateListing";
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -11,11 +12,12 @@ function App() {
     <Router>
       <div >
         <TopBar />
-        <Routes>
+        {/* <Header></Header> */}
+        {/* <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/detail/:id" element={<Detail />} />
           <Route exact path="/create" element={<CreateListing />} />
-        </Routes>
+        </Routes> */}
       </div>
     </Router>
   );
---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Header from '../header/Header';
+import './TopBar.css';
+import { NavLink } from 'react-router-dom';
+import pic from '../../images/wallpaperflare.com_wallpaper.jpg';
+
+//React component returns JSX kind on markup
+function TopBar() {
+    return (
+        <div>
+            <div className="topbar">
+                <div className="topbar-left">
+                    <i className="topbar-social-icon fab fa-instagram"></i>
+                    <i className="topbar-social-icon fab fa-facebook"></i>
+                    <i className="topbar-social-icon fab fa-linkedin"></i>
+                    <i className="topbar-social-icon fab fa-twitter"></i>
+                </div>
+                <div className="topbar-center">
+                    <ul className="topbar-list">
+                        <li className="topbar-list-item">
+                            <NavLink to="/">HOME</NavLink>
+                        </li>
+                        <li className="topbar-list-item">ABOUT</li>
+                        <li className="topbar-list-item">CONTACT</li>
+                        <li className="topbar-list-item"><NavLink to="/create">CREATE PROPERTY</NavLink></li>
+                        <li className="topbar-list-item">LOGOUT</li>
+                    </ul>
+                </div>
+                <div className="topbar-right">
+                    <img className="topbar-profile-pic" src={pic} alt="this is car image" />
+                    <i className="topbar-search fas fa-search"></i>
+                </div>
+            </div>
+            <div>
+                <Header />
+            </div>
+        </div>
+    )
+}
+
+export default TopBar
